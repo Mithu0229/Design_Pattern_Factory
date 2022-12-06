03d94a1f1b2b19d1a8be980a5664b8015cea7eec
Mithu0229 <showkat131080@gmail.com>
2022-12-06 10:32:30 +0600

    Add project files.

---
+++ Design_Pattern_Factory/ICreditCard.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_Factory
+{
+    // different type of class
+    class MoneyBack : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+
+    public class Titanium : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+    }
+
+
+    public class Platinum : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+    }
+
+
+    // interface
+    public interface ICreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+}
---
+++ Design_Pattern_Factory/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+//Factory is a class with a method. That method will create and return different types of objects based on the input parameter
+
+namespace Design_Pattern_Factory
+{
+    // create factory
+
+  public  class CreditCardFactory
+    {
+        public static ICreditCard GetCreditCard(string cardType)
+        {
+            ICreditCard cardDetails = null;
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+            return cardDetails;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreditCard cardDetails = CreditCardFactory.GetCreditCard("Platinum");
+
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.ReadLine();
+        }
+    }
+}
